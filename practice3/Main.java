582d618f01890f6221fe16da731d9a40109ae91a
mocchio <skmt.rome@gmail.com>
2023-05-02 13:01:53 +0900

    for文の練習

--- practice3/Main.java
+++ practice3/Main.java
@@ -12,22 +12,37 @@ public class Main {
     // }
     // System.out.println("ごちそうさまでした");
 
-    // switch文の練習
-    System.out.println("[メニュー] 1:検索 2:登録 3:削除 4:変更");
-    int selected = new java.util.Scanner(System.in).nextInt();
-    switch (selected) {
-      case 1:
-        System.out.println("検索します");
+    // // switch文の練習
+    // System.out.println("[メニュー] 1:検索 2:登録 3:削除 4:変更");
+    // int selected = new java.util.Scanner(System.in).nextInt();
+    // switch (selected) {
+    //   case 1:
+    //     System.out.println("検索します");
+    //     break;
+    //   case 2:
+    //     System.out.println("登録します");
+    //     break;
+    //   case 3:
+    //     System.out.println("削除します");
+    //     break;
+    //   case 4:
+    //     System.out.println("変更します");
+    //     break;
+    // }
+
+    // for文の練習
+    System.out.println("【数当てゲーム】");
+    int ans = new java.util.Random().nextInt(10);
+    for (int i = 0; i < 5; i ++) {
+      System.out.println("0~9の数字を入力してください");
+      int num = new java.util.Scanner(System.in).nextInt();
+      if (ans == num) {
+        System.out.println("アタリ！");
         break;
-      case 2:
-        System.out.println("登録します");
-        break;
-      case 3:
-        System.out.println("削除します");
-        break;
-      case 4:
-        System.out.println("変更します");
-        break;
+      } else {
+        System.out.println("違います");
+      }
     }
+    System.out.println("ゲームを終了します");
   }
 }
