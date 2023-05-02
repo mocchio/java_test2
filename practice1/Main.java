f489cc1e6e5d6da22dd2837cfe042a341e17de91
mocchio <skmt.rome@gmail.com>
2023-05-02 11:34:37 +0900

    変数の型練習

--- practice1/Main.java
+++ practice1/Main.java
@@ -5,5 +5,12 @@ public class Main {
     int b = 5;
     int c = a * b;
     System.out.println("縦幅" + a + "横幅" + b + "の長方形の面積は、" + c);
+
+    // 変数の型練習
+    boolean isError = true;
+    char name  = '駆';
+    double pi = 3.14;
+    long number = 314159265853979L;
+    String sentence = "ミナトの攻撃";
   }
 }
