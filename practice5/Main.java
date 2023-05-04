ca1f8a21309d1ed20937e3bb7721cd9204bd8a2b
mocchio <skmt.rome@gmail.com>
2023-05-04 16:07:44 +0900

    引数を含めたメソッドの練習

--- practice5/Main.java
+++ practice5/Main.java
@@ -1,6 +1,10 @@
 public class Main {
   public static void main(String[] args) {
+    String title = "新規物件のご案内";
+    String address = "test@test.com";
+    String text = "いつもお世話になっております。新規物件のご紹介です。";
     introduceOneself();
+    email(title, address, text);
   }
 
   // introduceOneselfメソッドの定義
@@ -11,5 +15,12 @@ public class Main {
     char zodiac = '寅';
     System.out.println("私の名前は" + name + "です。年齢は" + age + "歳です。");
     System.out.println("身長は" + height + "cm、干支は" + zodiac + "です。");
+  }
+
+  // emailメソッドの定義
+  public static void email(String title, String address, String text) {
+    System.out.println(address + "宛に以下のメールを送信しました。");
+    System.out.println("件名：" + title);
+    System.out.println("本文：" + text);
   }
 }
