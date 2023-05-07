267d1de6e576e06e4d2c8d0b6b6216463c1117e7
mocchio <skmt.rome@gmail.com>
2023-05-07 12:50:51 +0900

    Zenhanクラスの設定

---
+++ practice6/Zenhan.java
@@ -0,0 +1,11 @@
+package comment;
+
+public class Zenhan {
+  public static void doWarusa() {
+    System.out.println("きなこでござる。食えませんがの。");
+  }
+
+  public static void toTogame() {
+    System.out.println("この老ぼれの目は誤魔化せませんぞ");
+  }
+}
