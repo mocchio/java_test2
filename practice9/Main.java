7b196d692cde977e3fab5581767c8a9ed433cc0a
mocchio <skmt.rome@gmail.com>
2023-05-09 12:06:20 +0900

    コンストラクタ検証

---
+++ practice9/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void main(String[] args) {
+    Thief t = new Thief("勇者");
+    System.out.println("名前は" + t.name);
+    System.out.println("hpは" + t.hp);
+    System.out.println("mpは" + t.mp);
+  }
+}
