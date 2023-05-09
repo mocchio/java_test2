0e3851bdc982939babdda56a69f55509983db527
mocchio <skmt.rome@gmail.com>
2023-05-09 15:40:56 +0900

    インターフェースの実装

--- practice11/TangibleAsset.java
+++ practice11/TangibleAsset.java
@@ -1,6 +1,9 @@
-public abstract class TangibleAsset extends Asset {
+public abstract class TangibleAsset extends Asset implements Thing {
   // フィールドの設定
   String color;
+  
+  // フィールドの実装（継承元：Thing）
+  double weight;
 
   // コンストラクタの設定
   public TangibleAsset(String name, int price, String color) {
@@ -11,5 +14,13 @@ public abstract class TangibleAsset extends Asset {
   // メソッドの設定
   public String getColor() {
     return this.color;
+  }
+
+  // メソッドの実装（継承元：Thing）
+  public double getWeight() {
+    return this.weight;
+  }
+  public void setWeight(double weight) {
+    this.weight = weight;
   }
 }
